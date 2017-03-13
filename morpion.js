b31790f882870e30fc04be2f7ea2e7da70d3e636
kev-landry <kev.landry@free.fr>
2017-03-13 16:06:24 +0100

    first

---
+++ morpion.js
@@ -0,0 +1,17 @@
+function check(){
+
+  var i= 0;
+  var tab = [];
+  while (tab[i]) {
+    if (tab[i] == 'X' OR tab[i] == 'O') {
+
+    }
+  }
+}
+
+[[x,o,x],[x,o,o],[o,x,x]]
+tab[1][2]  ligne 2 colonne 3
+
+function grille(){
+
+}
---
+++ morpion.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Morpion</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="morpion.css">
+
+  </head>
+  <body>
+
+    <h1>Morpion</h1>
+
+
+    <table class="table">
+      <tr>
+        <td><button onclick="check()"></button></td>
+        <td><button onclick="check()"></button></td>
+        <td><button onclick="check()"></button></td>
+      </tr>
+      <tr>
+        <td><button onclick="check()"></button></td>
+        <td><button onclick="check()"></button></td>
+        <td><button onclick="check()"></button></td>
+      </tr>
+      <tr>
+        <td><button onclick="check()"></button></td>
+        <td><button onclick="check()"></button></td>
+        <td><button onclick="check()"></button></td>
+      </tr>
+    </table>
+
+<p>Joueur Humain commencez !</p>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+<?php
+
+
+
+?>
+<script type="text/javascript">
+
+var x =0;
+var y = 0;
+
+var tab = [];
+var i = 0;
+while (tab[i]) {
+
+}
+
+
+
+</script>
